b915c7ca0d741a3dff7e024015f1a746745661d2
Neetish Raj <neetish.1si11cs069@gmail.com>
2017-10-05 08:00:15 +0530

    Add files via upload

---
+++ js/radius.js
@@ -0,0 +1,9 @@
+$(document).ready(function(){ 	
+						   
+						   
+	// radius Box
+	$('.search span ').css({"border-radius": "5px", "-moz-border-radius":"5px", "-webkit-border-radius":"5px"});
+	$('.content_resize  ').css({"border-radius": "5px", "-moz-border-radius":"5px", "-webkit-border-radius":"5px"});
+	// end radius Box 
+	 
+});	
