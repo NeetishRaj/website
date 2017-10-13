efeba61dfe745e7875617d672cb6ea6af1eb859a
Neetish Raj <neetish.1si11cs069@gmail.com>
2017-10-13 17:34:06 +0530

    Added navigation options

--- js/radius.js
+++ js/radius.js
@@ -1,9 +1,10 @@
-$(document).ready(function(){ 	
-						   
-						   
+$(document).ready(function(){
+
+
 	// radius Box
 	$('.search span ').css({"border-radius": "5px", "-moz-border-radius":"5px", "-webkit-border-radius":"5px"});
 	$('.content_resize  ').css({"border-radius": "5px", "-moz-border-radius":"5px", "-webkit-border-radius":"5px"});
-	// end radius Box 
-	 
-});	
+	$('.header_resize  ').css({"border-radius": "5px", "-moz-border-radius":"5px", "-webkit-border-radius":"5px"});
+	// end radius Box
+	
+});
