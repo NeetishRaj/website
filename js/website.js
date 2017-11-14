8d05e9a07c6ec3c33fc2ebf1d264dd93e07c7cde
Neetish Raj <neetish.1si11cs069@gmail.com>
2017-11-15 05:02:34 +0530

    Added Night mode read button to all pages

--- js/website.js
+++ js/website.js
@@ -35,9 +35,35 @@ window.onload=function(){
   };
 
   //Night mode function for blog
-  document.getElementById('nightmode').onclick=function(){
-    document.querySelectorAll('.content_resize, .header_resize').style.backgroundColor="#000";
-    document.querySelectorAll('.main').style.color="#fff";
-    console.log("night mode");
+  var nightMode=document.getElementById('nightmode');
+  nightMode.onclick=function(){
+    var a=document.querySelectorAll('.main .article, .main .sidebar');
+    if(nightMode.innerHTML==="Night Mode"){
+      for (var i = 0; i < a.length; i++) {
+        a[i].style.backgroundColor="#253b40";
+        a[i].style.color="#ffea0d";
+      }
+      document.querySelector('.main').style.backgroundColor="#031727";
+      document.querySelector('.content_resize').style.backgroundColor="#253b40";
+      document.querySelector('.header').style.backgroundColor="#0a1827";
+      document.querySelector('.fbg_resize').style.backgroundColor="#0a1827";
+      nightmode.style.background="linear-gradient(#031727,#211210)";
+      nightmode.style.border="3px inset #122a22";
+      nightmode.style.color="#fbba42";
+      nightMode.innerHTML="Day Mode";
+    }else{
+      for (var i = 0; i < a.length; i++) {
+        a[i].style.backgroundColor="inherit";
+        a[i].style.color="inherit";
+      }
+      document.querySelector('.main').style.backgroundColor="inherit";
+      document.querySelector('.content_resize').style.backgroundColor="inherit";
+      document.querySelector('.header').style.backgroundColor="inherit";
+      document.querySelector('.fbg_resize').style.backgroundColor="inherit";
+      nightmode.style.background="linear-gradient(#c7c7c7,#fff)";
+      nightmode.style.border="3px outset #ccc";
+      nightmode.style.color="#4a4a4a";
+      nightMode.innerHTML="Night Mode";
+    }
   }
 };
