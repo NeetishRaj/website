78d7d92dfdbb421f082dde771442360aa515d330
Neetish Raj <neetish.1si11cs069@gmail.com>
2017-11-16 13:58:23 +0530

    added jumbotron animation. corrected night mode

--- MyTemplates/bootstrap/test.js
+++ MyTemplates/bootstrap/test.js
@@ -1,28 +1,52 @@
 //document.write("you are fucked");
 window.onload=function(){
+  var toggle=0;
+  var a=document.querySelector('.jumbotron');
+  var rotateY=0;
+  a.onmousemove=function(){
+    toggle=1;
+    var id=setInterval(frame,50);
+    var dx=1;
+    function frame(){
+      if(!toggle){
+        clearInterval(id);
+      }else{
+        if(rotateY===360){
+          toggle=0;
+          rotateY=0;
+        }
+        rotateY+=dx;
+        a.style.transform="rotateY("+rotateY+"deg)";
+      }
+    }
+  }
+  
 
-  var a=document.lastModified;
-  console.log(a);
-  var b=document.getElementById('modifiedDate');
-  b.innerHTML="Last Modified on : "+a;
-  var c=document.cokie;
-  console.log(c);
-  var d=document.URL;
-  console.log(d);
-  var e=document.baseURI;
-  console.log(e);
-  var f=document.documentMode;
-  console.log(f);
-  var g=document.documentURI;
-  console.log(g);
-  var h=document.implementation;
-  console.log(h);
-  var i=document.readyState;
-  console.log(i);
-  var j=document.strictErrorChecking;
-  console.log(j);
-  var k=document.scripts;
-  console.log(k);
-  var l=document.head;
-  console.log(l);
+
+  // var a=document.lastModified;
+  // console.log(a);
+  // var b=document.getElementById('modifiedDate');
+  // b.innerHTML="Last Modified on : "+a;
+  // var c=document.cokie;
+  // console.log(c);
+  // var d=document.URL;
+  // console.log(d);
+  // var e=document.baseURI;
+  // console.log(e);
+  // var f=document.documentMode;
+  // console.log(f);
+  // var g=document.documentURI;
+  // console.log(g);
+  // var h=document.implementation;
+  // console.log(h);
+  // var i=document.readyState;
+  // console.log(i);
+  // var j=document.strictErrorChecking;
+  // console.log(j);
+  // var k=document.scripts;
+  // console.log(k);
+  // var l=document.head;
+  // console.log(l);
+
+
 };
--- js/website.js
+++ js/website.js
@@ -20,7 +20,8 @@ window.onload=function(){
       "https://i.imgur.com/Zx8OPvB.png",
       "https://i.imgur.com/38QsBeI.jpg",
       "https://i.imgur.com/0R0cMTp.jpg",
-      "https://i.imgur.com/Vf079dH.jpg"
+      "https://i.imgur.com/Vf079dH.jpg",
+      "https://i.imgur.com/FBkSLJQ.png"
     ];
     var c=count();
     var arrLen=imgArr.length;
@@ -37,16 +38,16 @@ window.onload=function(){
   //Night mode function for blog
   var nightMode=document.getElementById('nightmode');
   nightMode.onclick=function(){
-    var a=document.querySelectorAll('.main .article, .main .sidebar');
+    var a=document.querySelectorAll('.main');
     if(nightMode.innerHTML==="Night Mode"){
       for (var i = 0; i < a.length; i++) {
         a[i].style.backgroundColor="#253b40";
         a[i].style.color="#ffea0d";
       }
-      document.querySelector('.main').style.backgroundColor="#031727";
+      document.querySelector('.main').style.background="#031727";
       document.querySelector('.content_resize').style.backgroundColor="#253b40";
-      document.querySelector('.header').style.backgroundColor="#0a1827";
-      document.querySelector('.fbg_resize').style.backgroundColor="#0a1827";
+      document.querySelector('.fbg').style.background="#0a1827";
+      document.querySelector('.menu_nav').style.background="#253b40";
       nightmode.style.background="linear-gradient(#031727,#211210)";
       nightmode.style.border="3px inset #122a22";
       nightmode.style.color="#fbba42";
@@ -56,10 +57,10 @@ window.onload=function(){
         a[i].style.backgroundColor="inherit";
         a[i].style.color="inherit";
       }
-      document.querySelector('.main').style.backgroundColor="inherit";
-      document.querySelector('.content_resize').style.backgroundColor="inherit";
-      document.querySelector('.header').style.backgroundColor="inherit";
-      document.querySelector('.fbg_resize').style.backgroundColor="inherit";
+      document.querySelector('.main').style.background="#eaeaea url(./best6gallery/main_bg.gif) top repeat-x";
+      document.querySelector('.content_resize').style.backgroundColor="#f6f4f4";
+      document.querySelector('.fbg').style.background="#5194bf url(./best6gallery/fbg_bg.gif) top repeat-x";
+      document.querySelector('.menu_nav').style.background="url(./best6gallery/menu_resize_bg.gif) top no-repeat";
       nightmode.style.background="linear-gradient(#c7c7c7,#fff)";
       nightmode.style.border="3px outset #ccc";
       nightmode.style.color="#4a4a4a";
@@ -67,3 +68,91 @@ window.onload=function(){
     }
   }
 };
+
+
+//text dazzle effect
+$(document).ready(function() {
+$(".focus").lettering();
+resetAnimation();
+});
+function resetAnimation() {
+  var myFocusText = $(".focus").text();
+  for (var i = 1; i <= myFocusText.length; i++) {
+      var randomNum = Math.random() * (2 - 0.5) + 0.5;
+      $(".char" + i).css({
+          animation: "textDazzle " + randomNum + "s ease-out infinite forwards alternate"
+      });
+  }
+}
+/*global jQuery */
+/*!
+* Lettering.JS 0.7.0
+*
+* Copyright 2010, Dave Rupert http://daverupert.com
+* Released under the WTFPL license
+* http://sam.zoy.org/wtfpl/
+*
+* Thanks to Paul Irish - http://paulirish.com - for the feedback.
+*
+* Date: Mon Sep 20 17:14:00 2010 -0600
+*/
+(function($){
+function injector(t, splitter, klass, after) {
+  var text = t.text()
+  , a = text.split(splitter)
+  , inject = '';
+  if (a.length) {
+    $(a).each(function(i, item) {
+      inject += '<span class="'+klass+(i+1)+'" aria-hidden="true">'+item+'</span>'+after;
+    });
+    t.attr('aria-label',text)
+    .empty()
+    .append(inject)
+
+  }
+}
+
+
+var methods = {
+  init : function() {
+
+    return this.each(function() {
+      injector($(this), '', 'char', '');
+    });
+
+  },
+
+  words : function() {
+
+    return this.each(function() {
+      injector($(this), ' ', 'word', ' ');
+    });
+
+  },
+
+  lines : function() {
+
+    return this.each(function() {
+      var r = "eefec303079ad17405c889e092e105b0";
+      // Because it's hard to split a <br/> tag consistently across browsers,
+      // (*ahem* IE *ahem*), we replace all <br/> instances with an md5 hash
+      // (of the word "split").  If you're trying to use this plugin on that
+      // md5 hash string, it will fail because you're being ridiculous.
+      injector($(this).children("br").replaceWith(r).end(), r, 'line', '');
+    });
+
+  }
+};
+
+$.fn.lettering = function( method ) {
+  // Method calling logic
+  if ( method && methods[method] ) {
+    return methods[ method ].apply( this, [].slice.call( arguments, 1 ));
+  } else if ( method === 'letters' || ! method ) {
+    return methods.init.apply( this, [].slice.call( arguments, 0 ) ); // always pass an array
+  }
+  $.error( 'Method ' +  method + ' does not exist on jQuery.lettering' );
+  return this;
+};
+
+})(jQuery);
