53ae1cab6d60c01721bdbac126d8f451d650c186
Neetish Raj <neetish.1si11cs069@gmail.com>
2017-12-27 11:20:51 +0530

    Completed all the side panel modules, just add Image module left

--- projects/image-editor/js/index.js
+++ projects/image-editor/js/index.js
@@ -15,25 +15,31 @@ $(function() {
     const SEPIA = "sepia(0%) ";
     const HUE_ROTATE = "hue-rotate(0deg) ";
 
-    const OUTER_WIDTH = window.innerWidth;
+    const OUTER_WIDTH = window.outerWidth;
     const INNER_HEIGHT = window.innerHeight;
     const CONTAINER_BORDER_WIDTH = 5;
     const SIDE_PANEL_WIDTH = 50;
     const MAX_WINDOW_WIDTH = 600;
     const HEADER_FOOTER_HEIGHT = 30;
     const CONTROL_PANEL_HEIGHT = 100;
-    const IMAGE_WIDTH = MAX_WINDOW_WIDTH - (2 * CONTAINER_BORDER_WIDTH + SIDE_PANEL_WIDTH);
+    const IMAGE_MINUS_WIDTH = (2 * CONTAINER_BORDER_WIDTH) + SIDE_PANEL_WIDTH;
+    const IMAGE_WIDTH = MAX_WINDOW_WIDTH - IMAGE_MINUS_WIDTH;
     const IMAGE_HEIGHT = INNER_HEIGHT - (2 * HEADER_FOOTER_HEIGHT + CONTROL_PANEL_HEIGHT);
 
-    $(".container").width(OUTER_WIDTH);
+    $(".container").width(OUTER_WIDTH - 2 * CONTAINER_BORDER_WIDTH);
     $(".container").height(INNER_HEIGHT);
-    // console.log(OUTER_WIDTH);
-    // console.log(INNER_HEIGHT);
+    // console.log(IMAGE_WIDTH);
+    // console.log((OUTER_WIDTH < IMAGE_WIDTH) ? (OUTER_WIDTH - SIDE_PANEL_WIDTH) : IMAGE_WIDTH);
 
-    $("#mainImageId").width((OUTER_WIDTH < IMAGE_WIDTH) ? OUTER_WIDTH - SIDE_PANEL_WIDTH : IMAGE_WIDTH);
+    $("#mainImageId").width((OUTER_WIDTH < IMAGE_WIDTH) ? OUTER_WIDTH - SIDE_PANEL_WIDTH - 2 * CONTAINER_BORDER_WIDTH: IMAGE_WIDTH + 2*CONTAINER_BORDER_WIDTH);
     $("#mainImageId").height(IMAGE_HEIGHT);
-    $("#mainImageId img").width((OUTER_WIDTH < IMAGE_WIDTH) ? OUTER_WIDTH - SIDE_PANEL_WIDTH : IMAGE_WIDTH);
+    $("#mainImageId img").width((OUTER_WIDTH < IMAGE_WIDTH) ? (OUTER_WIDTH - SIDE_PANEL_WIDTH - 2 * CONTAINER_BORDER_WIDTH) : IMAGE_WIDTH + 2*CONTAINER_BORDER_WIDTH);
     $("#mainImageId img").height(IMAGE_HEIGHT);
+
+    // $("#mainImageId").width(IMAGE_WIDTH);
+    // $("#mainImageId").height(IMAGE_HEIGHT);
+    // $("#mainImageId img").width(IMAGE_WIDTH);
+    // $("#mainImageId img").height(IMAGE_HEIGHT);
 
     var imageArray = [
         "https://i.imgur.com/wQ3uqBb.jpg",
@@ -54,6 +60,10 @@ $(function() {
             counter = 0;
         } else {
             $("#mainImageId img").attr("src", imageArray[counter++]);
+        }
+        $("#filterTypeText").html(" ");
+        for (var i = 0; i < imageArray.length; i++) {
+          $("<img />", {src:imageArray[i]}).appendTo("#filterTypeText");
         }
     });
 
@@ -129,6 +139,98 @@ $(function() {
         objectFitmode = "cover";
       }
 
+    });
+
+    $("#texture").on("click", function() {
+      // console.log("HI");
+      $("#texturePanelContainer").css({
+        "z-index" : "1",
+        "opacity" : "1"
+      });
+    });
+
+    //TEXTURE CLASS
+    function GradientTexture(bg, bgColor, bgImage, bgSize, bgPosition) {
+      this.background = bg;
+      this.backgroundColor = bgColor;
+      this.backgroundImage = bgImage;
+      this.backgroundSize = bgSize;
+      this.backgroundPosition = bgPosition;
+
+      this.applyChangesToTexture = function(){
+        $("#mainImageId, .texturePanel:last-child").css({
+          "background" : this.background,
+          "background-color" : this.backgroundColor,
+          "background-size" : this.backgroundSize,
+          "background-position" : this.backgroundPosition,
+          "background-image" : this.backgroundImage
+        });
+        $("#mainImageId img").css({"filter" : "filter(0.7)"});
+
+        $("#filterTypeText").html(
+          "img{filter: opacity(0.7); }<br>"+
+          ".container{<br>"+
+          "background: "+this.background+";<br>"+
+          "background-color: "+this.backgroundColor+";<br>"+
+          "background-size: "+this.backgroundSize+";<br>"+
+          "background-position: "+this.backgroundPosition+";<br>"+
+          "background-image: "+this.backgroundImage+"; };"
+        );
+      };
+    };
+
+    var texture1 = new GradientTexture(
+      "radial-gradient(hsl(0, 100%, 27%) 4%, hsl(0, 100%, 18%) 9%, hsla(0, 100%, 20%, 0) 9%) 0 0, radial-gradient(hsl(0, 100%, 27%) 4%, hsl(0, 100%, 18%) 8%, hsla(0, 100%, 20%, 0) 10%) 50px 50px, radial-gradient(hsla(0, 100%, 30%, 0.8) 20%, hsla(0, 100%, 20%, 0)) 50px 0, radial-gradient(hsla(0, 100%, 30%, 0.8) 20%, hsla(0, 100%, 20%, 0)) 0 50px, radial-gradient(hsla(0, 100%, 20%, 1) 35%, hsla(0, 100%, 20%, 0) 60%) 50px 0, radial-gradient(hsla(0, 100%, 20%, 1) 35%, hsla(0, 100%, 20%, 0) 60%) 100px 50px, radial-gradient(hsla(0, 100%, 15%, 0.7), hsla(0, 100%, 20%, 0)) 0 0, radial-gradient(hsla(0, 100%, 15%, 0.7), hsla(0, 100%, 20%, 0)) 50px 50px,linear-gradient(45deg, hsla(0, 100%, 20%, 0) 49%, hsla(0, 100%, 0%, 1) 50%, hsla(0, 100%, 20%, 0) 70%) 0 0, linear-gradient(-45deg, hsla(0, 100%, 20%, 0) 49%, hsla(0, 100%, 0%, 1) 50%, hsla(0, 100%, 20%, 0) 70%) 0 0",
+      "#300",
+      " ",
+      "100px 100px",
+      " "
+    );
+    $("#texture1").click(function(){
+      texture1.applyChangesToTexture();
+    });
+    var texture2 = new GradientTexture(
+      " ",
+      "#C8D3A7",
+      "radial-gradient(closest-side, transparent 0%, transparent 75%, #B6CC66 76%, #B6CC66 85%, #EDFFDB 86%, #EDFFDB 94%, #FFFFFF 95%, #FFFFFF 103%, #D9E6A7 104%, #D9E6A7 112%, #798B3C 113%, #798B3C 121%, #FFFFFF 122%, #FFFFFF 130%, #E0EAD7 131%, #E0EAD7 140%), radial-gradient(closest-side, transparent 0%, transparent 75%, #B6CC66 76%, #B6CC66 85%, #EDFFDB 86%, #EDFFDB 94%, #FFFFFF 95%, #FFFFFF 103%, #D9E6A7 104%, #D9E6A7 112%, #798B3C 113%, #798B3C 121%, #FFFFFF 122%, #FFFFFF 130%, #E0EAD7 131%, #E0EAD7 140%)",
+      "110px 110px",
+      "0 0, 55px 55px"
+    );
+    $("#texture2").click(function(){
+      texture2.applyChangesToTexture();
+    });
+
+    var texture3 = new GradientTexture(
+      " ",
+      "#556",
+      "linear-gradient(30deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), linear-gradient(150deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), linear-gradient(30deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), linear-gradient(150deg, #445 12%, transparent 12.5%, transparent 87%, #445 87.5%, #445), linear-gradient(60deg, #99a 25%, transparent 25.5%, transparent 75%, #99a 75%, #99a), linear-gradient(60deg, #99a 25%, transparent 25.5%, transparent 75%, #99a 75%, #99a)",
+      "80px 140px",
+      "0 0, 0 0, 40px 70px, 40px 70px, 0 0, 40px 70px"
+    );
+    $("#texture3").click(function(){
+      texture3.applyChangesToTexture();
+    });
+
+    var texture4 = new GradientTexture(
+      "linear-gradient(315deg, transparent 75%, #d45d55 0)-10px 0, linear-gradient(45deg, transparent 75%, #d45d55 0)-10px 0, linear-gradient(135deg, #a7332b 50%, transparent 0) 0 0, linear-gradient(45deg, #6a201b 50%, #561a16 0) 0 0 #561a16",
+      " ",
+      " ",
+      "20px 20px",
+      " "
+    );
+    $("#texture4").click(function(){
+      texture4.applyChangesToTexture();
+    });
+
+    var texture5 = new GradientTexture(
+      "linear-gradient(45deg, #dca 12%, transparent 0, transparent 88%, #dca 0), linear-gradient(135deg, transparent 37%, #a85 0, #a85 63%, transparent 0), linear-gradient(45deg, transparent 37%, #dca 0, #dca 63%, transparent 0) #753",
+      " ",
+      " ",
+      "25px 25px",
+      " "
+    );
+    $("#texture5").click(function(){
+      texture5.applyChangesToTexture();
     });
 
     $("#about").on("click", function() {
@@ -258,8 +360,9 @@ $(function() {
           $("#filterTypeText").html("img { transform: scaleY(-1);}");
         },
         translate : function(){
-          $("#mainImageId img").css({"transform" : "scaleX(-1) scaleY(-1)"});
-          $("#filterTypeText").html("img { transform: scaleX(-1) scaleY(-1);}");
+          $("#mainImageId img").css({"transform" : "scale(2)"});
+          $("#mainImageId").css({"overflow" : "scroll"});
+          $("#filterTypeText").html("img { transform: scale(2);}<br> .container img{ overflow: scroll; }");
         },
         rotate_left : function(){
           $("#mainImageId img").css({"transform" : "rotate(-90deg)"});
